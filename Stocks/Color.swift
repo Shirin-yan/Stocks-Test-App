4e228170387fd7cf38c92dc359e8b86f0a2fb10b
Shirin <shirin@Shirins-MacBook-Pro.local>
2023-10-18 19:42:17 +0500

    Colors added

---
+++ Stocks/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  Stocks
+//
+//  Created by Shirin on 18.10.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let bg = Color("BgColor")
+    static let darkBlue = Color("DarkBlueColor")
+    static let gray = Color("GrayColor")
+    static let red = Color("RedColor")
+    static let green = Color("GreenColor")
+}
