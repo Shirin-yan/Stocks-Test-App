a0502e67589aa1fa04d8d5f7b9927b8f779ff277
Shirin <shirin@Shirins-MacBook-Pro.local>
2023-10-18 18:59:11 +0500

    Initial commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Shirin on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
